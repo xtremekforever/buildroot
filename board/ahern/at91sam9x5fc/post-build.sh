d05edfa62edd438f75396cd0a36a26231e8a441b
Jesse L. Zamora <xtremekforever@gmail.com>
2013-11-07 19:50:57 -0500

    Update scripts to include kernel module and app in buildroot file

--- board/ahern/at91sam9x5fc/post-build.sh
+++ board/ahern/at91sam9x5fc/post-build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 FC_DIR="board/ahern/at91sam9x5fc"
-LINUX_DIR="output/build/linux-custom"
+BIN_DIR="../ahern-fc/build-arm"
+LINUX_DIR="../linux"
 
-echo -n "Copying Ahern Pump driver..."
-cp $LINUX_DIR/drivers/misc/ahern_pump.ko $1/root
+echo "Copying Ahern FC files..."
+
+echo -n "/etc/init.d/S30network_pre: " 
+cp $FC_DIR/S30network_pre $1/etc/init.d/
+chmod 755 $1/etc/init.d/S30network_pre
 if [ $? = 0 ]; then
 	echo "Done."
 else
   echo "Failed."
 fi
 
-echo -n "Copying Ahern FC initialization script..."
+echo -n "/etc/init.d/S60ahern: "
 cp $FC_DIR/S60ahern $1/etc/init.d/
 chmod 755 $1/etc/init.d/S60ahern
 if [ $? = 0 ]; then
@@ -20,11 +24,28 @@ else
   echo "Failed."
 fi
 
-echo -n "Copying default interfaces file..."
-cp -f $FC_DIR/interfaces $1/etc/network
+echo -n "/root/network-setup: " 
+cp $FC_DIR/network-setup $1/root
+chmod 755 $1/root/network-setup
 if [ $? = 0 ]; then
 	echo "Done."
 else
   echo "Failed."
 fi
 
+echo -n "/root/ahern_pump.ko: "
+cp $LINUX_DIR/drivers/misc/ahern_pump.ko $1/root
+if [ $? = 0 ]; then
+	echo "Done."
+else
+  echo "Failed."
+fi
+
+echo -n "/root/ahern-fc: "
+cp $BIN_DIR/ahern-fc $1/root
+chmod 755 $1/root/ahern-fc
+if [ $? = 0 ]; then
+	echo "Done."
+else
+  echo "Failed."
+fi
